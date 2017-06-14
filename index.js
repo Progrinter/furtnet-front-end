6fab592e137fb7f15f3df2338642a056e5bc74cb
GCostaSM <gabriel.mei@uol.com.br>
2017-06-13 23:55:13 -0300

    renomeando arquivos

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+
+var app = express();
+   app.set('view engine','ejs');
+   app.set("views","./");
+  app.use(express.static(('./')));
+
+
+    app.get("/",function(req,res){
+        res.render("home");
+    });
+
+    app.get("/teste",function(req,res){
+        res.render("perfil-psicologo");
+    });
+
+
+app.listen('3030');
