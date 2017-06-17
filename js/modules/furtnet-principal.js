5f4c8eecab4caa4c07b747ba70a1132da232c19d
GCostaSM <gabriel.mei@uol.com.br>
2017-06-17 00:03:39 -0300

    adicionando estados e cidades nos selects

--- js/modules/furtnet-principal.js
+++ js/modules/furtnet-principal.js
@@ -1,13 +1,16 @@
 angular.module("appfurtnet",['ngRoute'])
 .config(function($routeProvider){
 
+
     $routeProvider.when('/',{
         templateUrl: 'js/directives/usuario-painel.html', 
     });
 
+
     $routeProvider.when('/psicologo/new',{
         templateUrl: 'js/directives/psicologo-painel.html', 
     });
+    
 
     $routeProvider.when('/psicologo/perf',{
         templateUrl: 'js/directives/perfil-psicologo.html', 
