592cc1a37c968768438676575db75ea9a156a75c
PauloHenrique1994 <paulohenriquepaiva@ymail.com>
2017-10-13 16:44:31 -0300

    Alteracao back end

--- cadastro-usuario.php
+++ cadastro-usuario.php
@@ -13,13 +13,8 @@
 	//Select para procura de duplicidade
 	$result = mysqli_query($connection,$select);
 	$lista = mysqli_fetch_row($result);
-	$cont = 0;
 
-	foreach ($lista as $key => $value) {
-		$cont++;
-	}
-
-	if($cont >= 1) {
+	if($lista >= 1) {
 		echo "<p>O email ja esta cadastrado</p>";
 	}else{
 		$query = "INSERT INTO usuario (nome, genero, data, email, senha, tipo) values ('$nome', '$genero', '$data', '$email', '$senha', '$tipo')";
