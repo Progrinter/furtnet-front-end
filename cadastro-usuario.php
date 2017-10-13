4f345ba8c2f74c53241f3c7d89a07c0a537ef4ab
PauloHenrique1994 <paulohenriquepaiva@ymail.com>
2017-10-13 15:44:06 -0300

    Alteração de Páginas
    Restrição de cadastro no email, mudança provisioria no layout do site

--- cadastro-usuario.php
+++ cadastro-usuario.php
@@ -1,11 +1,29 @@
 <?php
 	include "cdb/config.php";
-	include "cdb/dados-user.php";
+
+	$nome = $_POST['nome'];
+	$genero = $_POST['genero'];
+	$data = $_POST['data'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
 
 	$connection = mysqli_connect($host, $login, $pass, $db);
-	$query = "INSERT into usuario (nome, cpf, genero, data, email, senha) values ('{$nome}', '{$cpf}', '{$genero}', '{$data}', '{$email}', '{$senha}')";
+	$select = "SELECT email FROM usuario WHERE email='$email'";
 
-	mysqli_query($connection,$query);
+	//Select para procura de duplicidade
+	$result = mysqli_query($connection,$select);
+	$lista = mysqli_fetch_row($result);
+	$cont = 0;
 
-	header("Location:laudo.php");	
+	foreach ($lista as $key => $value) {
+		$cont++;
+	}
+
+	if($cont >= 1) {
+		echo "<p>O email ja esta cadastrado</p>";
+	}else{
+		$query = "INSERT INTO usuario (nome, genero, data, email, senha, tipo) values ('$nome', '$genero', '$data', '$email', '$senha', '$tipo')";
+		mysqli_query($connection,$query);
+		header("Location:laudo.php");
+	}
 ?>
--- cdb/config.php
+++ cdb/config.php
@@ -1,7 +1,13 @@
 <?php
-	$host = "50.116.87.170";
+	/*$host = "50.116.87.170";
 	$db = "progr740_baseteste";
 	$login = "progr740";
 	$pass = "Prog@2017";
+	$table = "usuario";*/
+
+	$host = "127.0.0.1";
+	$db = "furtnet";
+	$login = "root";
+	$pass = "";
 	$table = "usuario";
 ?>
